1ed3ae183d4ce17953410af20f82a160acd26ff9
trexxet <atomnik07@gmail.com>
2017-06-17 11:03:34 +0000

    Moved init

---
+++ mods/vanilla/lua/map.lua
@@ -0,0 +1,8 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: Admin
+-- Date: 17.06.2017
+-- Time: 11:01
+-- To change this template use File | Settings | File Templates.
+--
+
--- src/main.cpp
+++ src/main.cpp
@@ -4,38 +4,43 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+int init (SDL_Window **window, SDL_Surface **screenSurface);
+
 int main (int argc, char *args[])
 {
   SDL_Window *window = NULL;
-
   SDL_Surface *screenSurface = NULL;
 
-  printf ("En Taro Adun");
+  if (!init(&window, &screenSurface))
+    SDL_Quit();
 
+  SDL_FillRect (screenSurface, NULL, SDL_MapRGB (screenSurface->format, 0xFF, 0xFF, 0xFF));
+  SDL_UpdateWindowSurface (window);
+  SDL_Delay (2000);
+
+  SDL_DestroyWindow (window);
+  SDL_Quit ();
+
+  return 0;
+}
+
+int init (SDL_Window **window, SDL_Surface **screenSurface)
+{
   if (SDL_Init (SDL_INIT_VIDEO) < 0)
   {
-    printf ("SDL could not initialize! SDL_Error: %s\n", SDL_GetError ());
+    printf ("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    return 1;
   }
   else
   {
-    window = SDL_CreateWindow ("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+    *window = SDL_CreateWindow ("Mens et Manus", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
                                SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (window == NULL)
+    if (*window == NULL)
     {
-      printf ("Window could not be created! SDL_Error: %s\n", SDL_GetError ());
-    }
-    else
-    {
-      screenSurface = SDL_GetWindowSurface (window);
-      SDL_FillRect (screenSurface, NULL, SDL_MapRGB (screenSurface->format, 0xFF, 0xFF, 0xFF));
-      SDL_UpdateWindowSurface (window);
-      SDL_Delay (2000);
+      printf ("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+      return 1;
     }
   }
-
-  SDL_DestroyWindow (window);
-
-  SDL_Quit ();
-
+  *screenSurface = SDL_GetWindowSurface (*window);
   return 0;
 }
