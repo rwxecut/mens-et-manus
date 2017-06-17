3b431ce42123e074bdb251c9f41dcac21b67a55f
trexxet <atomnik07@gmail.com>
2017-06-17 13:20:20 +0000

    Fixed init

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int init (SDL_Window **window, SDL_Surface **screenSurface);
+bool init (SDL_Window **window, SDL_Surface **screenSurface);
 
 int main (int argc, char *args[])
 {
   SDL_Window *window = NULL;
   SDL_Surface *screenSurface = NULL;
 
-  if (!init(&window, &screenSurface))
+  if (!init (&window, &screenSurface))
     SDL_Quit();
 
   SDL_FillRect (screenSurface, NULL, SDL_MapRGB (screenSurface->format, 0xFF, 0xFF, 0xFF));
@@ -24,23 +24,20 @@ int main (int argc, char *args[])
   return 0;
 }
 
-int init (SDL_Window **window, SDL_Surface **screenSurface)
+bool init (SDL_Window **window, SDL_Surface **screenSurface)
 {
   if (SDL_Init (SDL_INIT_VIDEO) < 0)
   {
-    printf ("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
-    return 1;
+    printf ("SDL could not initialize! SDL_Error: %s\n", SDL_GetError ());
+    return false;
   }
-  else
+  *window = SDL_CreateWindow ("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
+                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (*window == NULL)
   {
-    *window = SDL_CreateWindow ("Mens et Manus", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
-                               SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
-    if (*window == NULL)
-    {
-      printf ("Window could not be created! SDL_Error: %s\n", SDL_GetError());
-      return 1;
-    }
+    printf ("Window could not be created! SDL_Error: %s\n", SDL_GetError ());
+    return false;
   }
   *screenSurface = SDL_GetWindowSurface (*window);
-  return 0;
+  return true;
 }
